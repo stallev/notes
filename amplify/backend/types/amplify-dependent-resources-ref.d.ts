e67bf77f29ded828b3aefc6c6345de98a8e8c2ee
Alexander <stallev@yahoo.com>
2022-09-26 22:47:26 +0300

    added logic

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "notes": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "notesStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
